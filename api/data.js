370b725d5f7d1916c1b87ae31cabde9cea5ef2dd
sajkl <137759876+sajkl@users.noreply.github.com>
2025-12-17 00:13:58 +0300

    Create data.js

---
+++ api/data.js
@@ -0,0 +1,9 @@
+
+const CASES = {
+  jiga:  { title: 'Жига',  price: 249,  img: '/assets/case-1.png' },
+  camry: { title: 'Камри', price: 499,  img: '/assets/case-2.png' },
+  bmw:   { title: 'Бэха',  price: 999,  img: '/assets/case-3.png' },
+  lambo: { title: 'Ламбо', price: 2999, img: '/assets/case-4.png' }
+};
+
+module.exports = { CASES };
