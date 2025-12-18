e50f67d53121222b0cd3391f47f3df0ec6201370
sajkl <137759876+sajkl@users.noreply.github.com>
2025-12-18 19:19:40 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
-// db.js
 const { Pool } = require('pg');
 
-// Глобальная переменная, которая "выживает" между вызовами одной и той же горячей функции
+// Глобальная переменная для пула соединений
 let pool;
 
 function getPool() {
@@ -9,9 +8,10 @@ function getPool() {
     pool = new Pool({
       connectionString: process.env.DATABASE_URL,
       ssl: {
-        rejectUnauthorized: false, // Для Neon это часто нужно
+        rejectUnauthorized: false, // Обязательно для Neon
       },
-      /
+      max: 1, // Лимит соединений для Serverless
+      idleTimeoutMillis: 3000,
       connectionTimeoutMillis: 2000,
     });
   }
